03c2b2456e4d5582897567eb5ee576fabd4f17d6
imaciej10 <imaciej10@users.noreply.github.com>
2024-05-26 11:06:11 +0200

    Add CopyWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -19,6 +20,14 @@ module.exports = {
       filename: `index.html`,
       template: "src/index.html",
       inject: "body",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/image/svg",
+          to: "images", // output path for images
+        },
+      ],
     }),
   ],
   module: {
